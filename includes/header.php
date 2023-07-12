8dc8e31c1b64a684a4ce690fbe28616233963ebc
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-07-12 00:22:08 -0600

    Update

--- includes/header.php
+++ includes/header.php
@@ -72,14 +72,14 @@ if ($usuario == null || $usuario == ''  && $permiso == null || $permiso == '') {
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>reportes</span>
+                    <span>Reportes</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Ver Modulos</h6>
-                        <a class="collapse-item" href="../views/inventario.php">reportes diarios </a>
-                        <a class="collapse-item" href="../views/r_mensuales.php">reportes mensuales </a>
-                        <a class="collapse-item" href="../views/horas_total.php">reporte hrs de maquinas</a>
+                        <a class="collapse-item" href="../views/inventario.php">Reportes diarios </a>
+                        <a class="collapse-item" href="../views/r_mensuales.php">Reportes mensuales </a>
+                        <a class="collapse-item" href="../views/horas_total.php">Reporte hrs de maquinas</a>
                         <!--  <a class="collapse-item" href="../views/codbarra.php">reporte del mes</a>-->
                     </div>
                 </div>
@@ -89,12 +89,12 @@ if ($usuario == null || $usuario == ''  && $permiso == null || $permiso == '') {
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
-                    <span>agregar nueva maquina</span>
+                    <span>Maquinas</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header"> </h6>
-                        <a class="collapse-item" href="../views/maquinas.php">nueva maquina</a>
+                        <a class="collapse-item" href="../views/maquinas.php">Nueva Maquina</a>
 
                     </div>
                 </div>
