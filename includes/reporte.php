3d36e014b599273d1fe44184f24d2ad2decbd528
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-07-14 19:12:29 -0600

    Add files via upload

--- includes/reporte.php
+++ includes/reporte.php
@@ -8,7 +8,6 @@ class PDF extends FPDF
     // Cabecera de página
     function Header()
     {
-
         //$this->image('', 150, 1, 40); // X, Y, Tamaño
         $this->Ln(20);
         // Arial bold 15
@@ -18,65 +17,63 @@ class PDF extends FPDF
         $this->Cell(100);
         // Título
         $this->setY(10);
-        $this->setX(105);
+        $this->setX(145);
 
         $this->Cell(70, 10, utf8_decode('REPORTE DE MAQUINARIAS '), 0, 1, 'C');
 
-
-
         $this->SetFont('Arial', '', 15);
         $this->setY(20);
-        $this->setX(110);
+        $this->setX(150);
         $this->Cell(60, 4, 'CONTROL DE MAQUINARIA', 0, 1, 'C');
 
-
-        $this->SetFont('Helvetica', '', 9);
+        $this->SetFont('Helvetica', '', 11);
         $this->Ln(20);
         $this->setY(45);
-        $this->setX(10);
+        $this->setX(25);
         $this->Cell(60, 0, 'Maquina: ', 0, 1, 'C');
 
-        $this->SetFont('Helvetica', '', 9);
+        $this->SetFont('Helvetica', '', 11);
         $this->Ln(20);
         $this->setY(45);
-        $this->setX(50);
+        $this->setX(75);
         $this->Cell(60, 0, 'Modelo: ', 0, 1, 'C');
 
-        $this->SetFont('Helvetica', '', 9);
+        $this->SetFont('Helvetica', '', 11);
         $this->Ln(20);
         $this->setY(45);
-        $this->setX(90);
+        $this->setX(115);
         $this->Cell(60, 0, 'Serie: ', 0, 1, 'C');
 
-        $this->SetFont('Helvetica', '', 9);
+        $this->SetFont('Helvetica', '', 11);
         $this->Ln(20);
         $this->setY(45);
-        $this->setX(130);
+        $this->setX(155);
         $this->Cell(60, 0, 'Ubicacion: ', 0, 1, 'C');
 
-        $this->SetFont('Helvetica', '', 9);
+        $this->SetFont('Helvetica', '', 11);
         $this->Ln(20);
         $this->setY(45);
-        $this->setX(170);
+        $this->setX(200);
         $this->Cell(60, 0, 'Estatus: ', 0, 1, 'C');
-        
-        $this->SetFont('Helvetica', '', 9);
+
+        $this->SetFont('Helvetica', '', 11);
         $this->Ln(20);
         $this->setY(45);
-        $this->setX(210);
+        $this->setX(250);
         $this->Cell(60, 0, 'Mantenimiento: ', 0, 1, 'C');
 
-        $this->SetFont('Helvetica', '', 9);
+        $this->SetFont('Helvetica', '', 11);
         $this->Ln(20);
         $this->setY(60);
-        $this->setX(18);
+        $this->setX(34);
         $this->Cell(60, 0, 'Total de HRS Activa: ', 0, 1, 'C');
 
-        $this->SetFont('Helvetica', '', 9);
+        $this->SetFont('Helvetica', '', 11);
         $this->Ln(20);
         $this->setY(60);
-        $this->setX(65);
+        $this->setX(95);
         $this->Cell(60, 0, 'Total de HRS Parada: ', 0, 1, 'C');
+
 
 
         $this->SetFont('Arial', 'B', 17);
@@ -94,22 +91,21 @@ class PDF extends FPDF
 
 
         $this->Ln();
-        $this->SetFont('Arial', 'B', 8);
-        $this->SetY(70);
+        $this->SetFont('Arial', 'B', 10);
+        $this->SetY(72);
         $this->SetX(10);
 
-        $this->Cell(30, 10, 'Operador', 1, 0, 'C', 0);
-        $this->Cell(23, 10, 'Fecha', 1, 0, 'C', 0,);
+        $this->Cell(40, 10, 'Operador', 1, 0, 'C', 0);
+        $this->Cell(25, 10, 'Fecha', 1, 0, 'C', 0,);
         $this->Cell(27, 10, 'Hrs Trab.', 1, 0, 'C', 0);
         $this->Cell(27, 10, 'Hrs In', 1, 0, 'C', 0);
         $this->Cell(23, 10, 'HRMT Ini', 1, 0, 'C', 0);
         $this->Cell(23, 10, 'HRMT Ter', 1, 0, 'C', 0);
-        $this->Cell(25, 10, 'Lugar', 1, 0, 'C', 0);
         $this->Cell(25, 10, 'Falla', 1, 0, 'C', 0);
-        $this->Cell(20, 10, 'Hra Paro', 1, 0, 'C', 0);
-        $this->Cell(20, 10, 'Hra Reinicio', 1, 0, 'C', 0);
-        $this->Cell(20, 10, 'Gastos', 1, 0, 'C', 0);
-        $this->Cell(22, 10, 'Observacion', 1, 1, 'C', 0);
+        $this->Cell(23, 10, 'Hra Paro', 1, 0, 'C', 0);
+        $this->Cell(23, 10, 'Hra Reinicio', 1, 0, 'C', 0);
+        $this->Cell(22, 10, 'Gastos', 1, 0, 'C', 0);
+        $this->Cell(78, 10, 'Observacion', 1, 1, 'C', 0);
     }
 
     // Pie de página
@@ -121,30 +117,12 @@ class PDF extends FPDF
         // Arial italic 8
         $this->SetFont('Arial', 'I', 8);
         // Número de página
-
-
-       
-
-
-
-
-        //$this->SetFillColor(223, 229,235);
-        //$this->SetDrawColor(181, 14,246);
-        //$this->Ln(0.5);
     }
 }
 
-
-
-$pdf = new PDF();
-$pdf = new PDF('L', 'mm', 'letter');
+$pdf = new PDF('L', 'mm', 'Legal');
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Arial', '', 0);
-
-//$pdf->SetWidths(array(10, 30, 27, 27, 20, 20, 20, 20, 22));
-
-
-
 
 $pdf->Output();
