49d0568e94843936c2ee62f6436f341939eebfc3
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-07-19 10:06:11 -0600

    UpdateIndex

--- views/index.php
+++ views/index.php
@@ -1,108 +1,141 @@
 <?php
 include "../includes/header.php";
 ?>
+
+<style>
+  .centered-img {
+    display: block;
+    margin: 0 auto;
+    margin-top: 60PX;
+  }
+</style>
+
+
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Bienvenido al Sistema de Control de Maquinaria</h1>
-      <!--  <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>-->
-      
-    </div>
-   <center><img src="Logo-Primario (1).png" style="max-height: 170px;"></center>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-</script>
+  <!-- Page Heading -->
 
-    <!-- Content Row -->
-    <div class="row">
+  <center>
+    <h1 class="h3 mb-0 text-gray-800">Bienvenido <?php echo $usuario; ?> al Sistema De Control De Maquinas</h1>
 
-        <!-- Earnings (Monthly) Card Example 
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Earnings (Monthly)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
+  </center>
+  <br>
+  <!-- Content Row -->
+  <div class="row">
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <a href="../views/operadores.php" class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                Operadores</a>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">
+                <?php
+                require_once("../includes/db.php");
+
+                $SQL = "SELECT id FROM operadores ORDER BY id";
+                $dato = mysqli_query($conexion, $SQL);
+                $fila = mysqli_num_rows($dato);
+
+                echo ($fila); ?>
+              </div>
             </div>
-        </div>-->
-
-        <!-- Earnings (Monthly) Card Example 
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Earnings (Annual)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-auto">
+              <i class="fas fa-calendar fa-2x text-gray-300"></i>
             </div>
-        </div>-->
-
-        <!-- Earnings (Monthly) Card Example 
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                            </div>
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                </div>
-                                <div class="col">
-                                    <div class="progress progress-sm mr-2">
-                                        <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>-->
-
-        <!-- Pending Requests Card Example 
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Pending Requests</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    </div>-->
+      </div>
+    </div>
 
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-success shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <a href="../views/maquinas.php" class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                Maquinas</a>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">
+                <?php
+                require_once("../includes/db.php");
+
+                $SQL = "SELECT id FROM maquinas ORDER BY id";
+                $dato = mysqli_query($conexion, $SQL);
+                $fila = mysqli_num_rows($dato);
+
+                echo ($fila); ?>
+              </div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Earnings (Monthly) Card Example -->
+
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-info shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <a href="../views/inventario.php" class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                Reporte General</a>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">
+                <?php
+                require_once("../includes/db.php");
+
+                $SQL = "SELECT id FROM inventario ORDER BY id";
+                $dato = mysqli_query($conexion, $SQL);
+                $fila = mysqli_num_rows($dato);
+
+                echo ($fila); ?>
+              </div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Pending Requests Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-warning shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <a href="#" class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                Usuarios</a>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">
+                <?php
+                require_once("../includes/db.php");
+
+                $SQL = "SELECT id FROM inventario ORDER BY id";
+                $dato = mysqli_query($conexion, $SQL);
+                $fila = mysqli_num_rows($dato);
+
+                echo ($fila); ?>
+              </div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-comments fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <center>
+    <img src="Logo-Primario (1).png" style="max-height: 170px;" class="centered-img">
+  </center>
 </div>
 </div>
 
